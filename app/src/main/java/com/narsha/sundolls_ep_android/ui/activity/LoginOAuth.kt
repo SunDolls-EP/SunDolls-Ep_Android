1c9e0c0d12419a8707c9b7c605ca3f1a7700cf1a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-04-21 19:32:47 +0900

    :construction: create LoginOAuth Activity, connection viewModel and connection dataBinding

---
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/LoginOAuth.kt
@@ -0,0 +1,18 @@
+package com.narsha.sundolls_ep_android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.narsha.sundolls_ep_android.R
+import com.narsha.sundolls_ep_android.databinding.ActivityLoginOauthBinding
+import com.narsha.sundolls_ep_android.ui.viewmodel.LoginOAuthViewModel
+
+class LoginOAuth : AppCompatActivity() {
+    private val viewModel: LoginOAuthViewModel by lazy { ViewModelProvider(this)[LoginOAuthViewModel::class.java] }
+    private val binding = DataBindingUtil.setContentView<ActivityLoginOauthBinding>(this, R.layout.activity_login_oauth)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.login = viewModel
+    }
+}
---
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/LoginOAuthViewModel.kt
@@ -0,0 +1,11 @@
+package com.narsha.sundolls_ep_android.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class LoginOAuthViewModel: ViewModel() {
+
+    fun nonMember(){
+
+    }
+
+}
