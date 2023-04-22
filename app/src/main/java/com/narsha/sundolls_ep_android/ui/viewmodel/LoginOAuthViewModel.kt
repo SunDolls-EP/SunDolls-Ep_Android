70576e890164bc683d58923f22b7f925579de977
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-04-22 22:55:05 +0900

    :goal_net: AppCompatTextView onClick test

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/LoginOAuthViewModel.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/LoginOAuthViewModel.kt
@@ -1,11 +1,13 @@
 package com.narsha.sundolls_ep_android.ui.viewmodel
 
+import android.content.Intent
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class LoginOAuthViewModel: ViewModel() {
 
-    fun nonMember(){
-
+    fun nonMemberGoHomeActivity(){
+        Log.d("상태", "Text 클릭")
     }
 
 }
