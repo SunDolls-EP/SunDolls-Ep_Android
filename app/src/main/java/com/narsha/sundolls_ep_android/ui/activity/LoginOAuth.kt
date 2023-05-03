28451a89b6d6a634c6737f62a81309ef0485b471
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-03 23:20:20 +0900

    :construction: google OAuth 2.0

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/LoginOAuth.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/LoginOAuth.kt
@@ -1,10 +1,21 @@
 package com.narsha.sundolls_ep_android.ui.activity
 
-import android.content.Intent
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.narsha.sundolls_ep_android.R
 import com.narsha.sundolls_ep_android.databinding.ActivityLoginOauthBinding
 import com.narsha.sundolls_ep_android.ui.viewmodel.LoginOAuthViewModel
@@ -12,7 +23,6 @@ import com.narsha.sundolls_ep_android.ui.viewmodel.LoginOAuthViewModel
 class LoginOAuth : AppCompatActivity() {
     private val viewModel: LoginOAuthViewModel by lazy { ViewModelProvider(this)[LoginOAuthViewModel::class.java] }
     private val binding: ActivityLoginOauthBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_login_oauth) }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +37,8 @@ class LoginOAuth : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         viewModel.goHome.observe(this) {
-            startActivity(Intent(this, Home::class.java))
-            finish()
+//            startActivity(Intent(this, Home::class.java))
+//            finish()
         }
     }
 
@@ -43,4 +53,7 @@ class LoginOAuth : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
     }
+
+
+
 }
--- app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/LoginOAuthViewModel.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/LoginOAuthViewModel.kt
@@ -1,11 +1,13 @@
 package com.narsha.sundolls_ep_android.ui.viewmodel
 
 import android.app.Activity
+import android.app.Application
 import android.content.Context
 import android.util.Log
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -21,46 +23,45 @@ class LoginOAuthViewModel: ViewModel() {
 
     private val _goHome = MutableLiveData<Boolean>()
 
-//    private var googleSignInClient: GoogleSignInClient
-
     private val _navigateToMainActivity = MutableLiveData<Boolean>()
-//    val navigateToMainActivity: LiveData<Boolean>
-//        get() = _navigateToMainActivity
-//    init {
-//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestEmail()
-//            .build()
-//
-//        googleSignInClient = GoogleSignIn.getClient(Activity().applicationContext, gso)
-//    }
-//
-//    fun signIn(context: Context) {
-//        var registry: ActivityResultRegistry?= null
-//        val signInIntent = googleSignInClient.signInIntent
-//        if (context is FragmentActivity) {
-//            registry = context.activityResultRegistry
-//        }
-//        val signInActivityResultLauncher = registry?.register(
-//            "key_sign_in",
-//            ActivityResultContracts.StartActivityForResult()) { result ->
-//            if (result.resultCode == Activity.RESULT_OK) {
-//                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-//                handleSignInResult(task)
-//            }
-//        }
-//        signInActivityResultLauncher?.launch(signInIntent)
-//    }
-//    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-//        try {
-//            val account = completedTask.getResult(ApiException::class.java)
-//
-//            // Signed in successfully, show authenticated UI.
-//            _navigateToMainActivity.value = true
-//        } catch (e: ApiException) {
-//            // The ApiException status code indicates the detailed failure reason.
-//            Log.w("LoginViewModel", "signInResult:failed code=${e.statusCode}")
-//        }
-//    }
+    val navigateToMainActivity: LiveData<Boolean>
+        get() = _navigateToMainActivity
+
+    fun signIn(context: Context) {
+        var googleSignInClient: GoogleSignInClient
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(context, gso)
+
+        var registry: ActivityResultRegistry?= null
+        val signInIntent = googleSignInClient.signInIntent
+        if (context is FragmentActivity) {
+            registry = context.activityResultRegistry
+        }
+        val signInActivityResultLauncher = registry?.register(
+            "key_sign_in",
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleSignInResult(task)
+            }
+        }
+        signInActivityResultLauncher?.launch(signInIntent)
+    }
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            Log.w("LoginViewModel", "signInResult:failed code=${e.statusCode}")
+        }
+    }
+
+
 
 
     val goHome: LiveData<Boolean>
