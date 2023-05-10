f3b8f3daf1bcaeb6833e0b1f48ccce4547406f8c
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-10 20:21:09 +0900

    :sparkles: Login Google (retrofit)

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/LoginOAuth.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/LoginOAuth.kt
@@ -1,67 +1,20 @@
 package com.narsha.sundolls_ep_android.ui.activity
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.Scope
-import com.google.android.gms.tasks.Task
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest
-import com.google.api.services.people.v1.PeopleServiceScopes
 import com.narsha.sundolls_ep_android.R
 import com.narsha.sundolls_ep_android.data.local.googleOAuth.LoginGoogle
 import com.narsha.sundolls_ep_android.databinding.ActivityLoginOauthBinding
 import com.narsha.sundolls_ep_android.ui.viewmodel.LoginOAuthViewModel
 
 class LoginOAuth : AppCompatActivity() {
-
-    companion object{
-        const val RC_SIGN_IN = 1000
-    }
-
-    private lateinit var googleSignInClient: GoogleSignInClient
-
-    private val signInResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                handleSignInResult(task)
-            } else {
-                Log.d("애러",result.toString())
-            }
-        }
-
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-            val idToken = account.idToken
-            Log.d("GoogleSignIn", "User name: ${account.displayName}")
-            Log.d("GoogleSignIn", "User email: ${account.email}")
-
-//            val tokenRequest = GoogleAuthorizationCodeTokenRequest(
-//                transport,
-//                jsonFactory,
-//                getString(R.),
-//                clientSecret,
-//                code,
-//                redirectUri)
-            // 로그인 성공 처리
-            // account.idToken으로 인증 정보를 가져올 수 있습니다.
-        } catch (e: ApiException) {
-            Log.d("상태",e.message.toString())
-            // 로그인 실패 처리
-        }
-    }
 
     private val viewModel: LoginOAuthViewModel by lazy { ViewModelProvider(this)[LoginOAuthViewModel::class.java] }
     private val binding: ActivityLoginOauthBinding by lazy {
@@ -71,24 +24,38 @@ class LoginOAuth : AppCompatActivity() {
         )
     }
 
+    private val loginGoogle = LoginGoogle(this)
+
+    private val googleSignInClient: GoogleSignInClient by lazy {
+        GoogleSignIn.getClient(this, loginGoogle.gso)
+    }
+
+
+
+    private val signInResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                loginGoogle.handleSignInResult(task)
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Log.d("애러",result.toString())
+            }
+        }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.login = viewModel
         binding.lifecycleOwner = this
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestIdToken(getString(R.string.client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-
         viewModel.goHome.observe(this) {
             val signInIntent = googleSignInClient.signInIntent
             signInResultLauncher.launch(signInIntent)
-            Log.d("client_id",getString(R.string.client_id))
         }
-
     }
 
     override fun onStart() {
