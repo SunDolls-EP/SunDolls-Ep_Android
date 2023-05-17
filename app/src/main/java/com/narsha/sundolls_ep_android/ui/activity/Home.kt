edab8e30bccd6f8feb2d094b1fa87351c78fbc06
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-17 18:06:44 +0900

    :wheelchair: Code to use the context in the activity

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/Home.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/Home.kt
@@ -1,6 +1,7 @@
 package com.narsha.sundolls_ep_android.ui.activity
 
 import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -10,6 +11,17 @@ import com.narsha.sundolls_ep_android.databinding.ActivityHomeBinding
 import com.narsha.sundolls_ep_android.ui.viewmodel.HomeViewModel
 
 class Home : AppCompatActivity() {
+
+    companion object{
+        lateinit var instance: Home
+        fun ApplicationContext() : Context {
+            return instance.applicationContext
+        }
+    }
+    init{
+        instance = this
+    }
+
     private val viewModel: HomeViewModel by lazy { ViewModelProvider(this)[HomeViewModel::class.java] }
     private val binding by lazy { DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home) }
     override fun onCreate(savedInstanceState: Bundle?) {
