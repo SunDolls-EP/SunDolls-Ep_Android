eff1dbe1aee357d724b0c197bb8a09ada267a6f0
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-24 21:29:36 +0900

    :sparkles: Attach ViewModel and Attach RecyclerView

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/FriendFragment.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/FriendFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.narsha.sundolls_ep_android.R
 import com.narsha.sundolls_ep_android.databinding.FragmentFriendBinding
+import com.narsha.sundolls_ep_android.ui.adapter.RecyclerViewFriendAdapter
 import com.narsha.sundolls_ep_android.ui.viewmodel.fragment.FriendViewModel
 
 
@@ -17,15 +20,28 @@ class FriendFragment : Fragment() {
         ViewModelProvider(this)[FriendViewModel::class.java]
     }
 
+    lateinit var binding: FragmentFriendBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        val binding: FragmentFriendBinding = FragmentFriendBinding.inflate(inflater, container, false)
+        binding = FragmentFriendBinding.inflate(inflater, container, false)
         binding.ranking = viewModel
+
+        binding.lifecycleOwner = this
+        binding.friendRecycler.layoutManager = LinearLayoutManager(context)
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.recyclerView.observe(viewLifecycleOwner, Observer {
+            var newAdapter = RecyclerViewFriendAdapter(it)
+            binding.friendRecycler.adapter = newAdapter
+        })
     }
 
 }
