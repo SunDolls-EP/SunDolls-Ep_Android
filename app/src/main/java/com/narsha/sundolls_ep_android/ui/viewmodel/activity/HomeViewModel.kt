94ef9e03a63fdc148bdf2935df7aa77a6bd7037a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-24 15:46:43 +0900

    :wastebasket: Delete unnecessary code

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/activity/HomeViewModel.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/activity/HomeViewModel.kt
@@ -2,36 +2,14 @@ package com.narsha.sundolls_ep_android.ui.viewmodel.activity
 
 import android.util.Log
 import android.view.Gravity
+import android.view.MenuItem
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.narsha.sundolls_ep_android.R
 import com.narsha.sundolls_ep_android.databinding.ActivityHomeBinding
 
 class HomeViewModel:ViewModel() {
 
 
-    private val _Check_Time = MutableLiveData<Int>()
-
-    private val _onclickDrawerLayout = MutableLiveData<Boolean>()
-    val onclickDrawerLayout: LiveData<Boolean>
-        get() = _onclickDrawerLayout
-
-    val Check_Time: LiveData<Int>
-        get() = _Check_Time
-
-    fun Time(time: Int) {
-        _Check_Time.value = time
-    }
-
-
-    fun OnclickDrawerLayout(){
-        _onclickDrawerLayout.value = true
-    }
-    fun OnclickDrawerOpen(binding: ActivityHomeBinding){
-        if (!binding.DrawerLayout.isDrawerOpen(Gravity.LEFT)) {
-            binding.DrawerLayout.openDrawer(Gravity.LEFT)
-        } else {
-            binding.DrawerLayout.closeDrawer(Gravity.LEFT)
-        }
-    }
 }
