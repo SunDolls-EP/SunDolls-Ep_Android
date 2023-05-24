974eb5dae7c572fbc5584767d98b07abf818b847
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-24 15:36:41 +0900

    :sparkles: Open Drawer Layout with a click of a button

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/fragment/HomeViewModel.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/fragment/HomeViewModel.kt
@@ -1,6 +1,33 @@
 package com.narsha.sundolls_ep_android.ui.viewmodel.fragment
 
+import android.view.Gravity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.narsha.sundolls_ep_android.databinding.ActivityHomeBinding
+import com.narsha.sundolls_ep_android.databinding.FragmentHomeBinding
+import com.narsha.sundolls_ep_android.ui.fragment.HomeFragment
 
-class HomeViewModel:ViewModel() {
+class HomeViewModel : ViewModel() {
+
+
+    private val _onclickDrawerLayout = MutableLiveData<Boolean>()
+    val onclickDrawerLayout: LiveData<Boolean>
+        get() = _onclickDrawerLayout
+
+
+
+    fun OnclickDrawerLayout() {
+        _onclickDrawerLayout.value = true
+    }
+
+
+
+    fun OnclickDrawerOpen(binding: FragmentHomeBinding) {
+        if (!binding.DrawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            binding.DrawerLayout.openDrawer(Gravity.LEFT)
+        } else {
+            binding.DrawerLayout.closeDrawer(Gravity.LEFT)
+        }
+    }
 }
