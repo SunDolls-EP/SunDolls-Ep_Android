f414ae34ac76e1dee3357a76b6dc15af31ac0e59
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-24 21:34:43 +0900

    :heavy_plus_sign: Connect ViewModel class, Log

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/Home.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/Home.kt
@@ -3,6 +3,7 @@ package com.narsha.sundolls_ep_android.ui.activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -13,6 +14,7 @@ import com.narsha.sundolls_ep_android.ui.fragment.FriendFragment
 import com.narsha.sundolls_ep_android.ui.fragment.HomeFragment
 import com.narsha.sundolls_ep_android.ui.fragment.RankingFragment
 import com.narsha.sundolls_ep_android.ui.viewmodel.activity.HomeViewModel
+import com.narsha.sundolls_ep_android.ui.viewmodel.fragment.FriendViewModel
 
 class Home : AppCompatActivity() {
 
