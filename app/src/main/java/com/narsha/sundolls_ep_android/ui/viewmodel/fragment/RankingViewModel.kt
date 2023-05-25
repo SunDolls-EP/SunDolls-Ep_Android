aebca3261986c66b5e18533bb4acb94c3374465b
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-25 16:43:06 +0900

    :construction: Check RecyclerView with LiveData

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/fragment/RankingViewModel.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/fragment/RankingViewModel.kt
@@ -1,7 +1,23 @@
 package com.narsha.sundolls_ep_android.ui.viewmodel.fragment
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.narsha.sundolls_ep_android.data.local.retrofit.response.friendLookupResponse.FriendLookupResponse
+import com.narsha.sundolls_ep_android.ui.adapter.RecyclerViewRankingData
+import kotlin.random.Random
 
 class RankingViewModel:ViewModel() {
+
+    private val _recyclerView = MutableLiveData<MutableList<RecyclerViewRankingData>>()
+    val recyclerView: LiveData<MutableList<RecyclerViewRankingData>>
+        get() = _recyclerView
+
+    fun getRanking(){
+        _recyclerView.value?.add(RecyclerViewRankingData("황주완",523))
+        Log.d("상태",_recyclerView.value.toString())
+        Log.d("상태",RecyclerViewRankingData("황주완",523).toString())
+    }
 
 }
