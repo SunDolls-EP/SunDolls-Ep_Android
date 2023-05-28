c4e68daceec7795386f565bde2098bee2bd64a8d
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-28 23:55:47 +0900

    :card_file_box: add time SharedPreferences

--- app/src/main/java/com/narsha/sundolls_ep_android/data/model/MySharedPreferences.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/data/model/MySharedPreferences.kt
@@ -6,13 +6,18 @@ import android.content.SharedPreferences
 class MySharedPreferences(context: Context) {
     companion object{
         const val PREFS_FILENAME = "prefs"
-        const val PREF_KEY_MY_EDITTEXT = "myEditText"
+        const val PREF_KEY_ACCESSTOKEN = "accessToken"
+        const val PREF_KEY_TIME = "time"
     }
 
     val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
-    var access_token: String?
-        get() = prefs.getString(PREF_KEY_MY_EDITTEXT, "")
-        set(value) = prefs.edit().putString(PREF_KEY_MY_EDITTEXT, value).apply()
+    var accessToken: String?
+        get() = prefs.getString(PREF_KEY_ACCESSTOKEN, "")
+        set(value) = prefs.edit().putString(PREF_KEY_ACCESSTOKEN, value).apply()
+
+    var time: Long
+        get() = prefs.getLong(PREF_KEY_TIME,0L)
+        set(value) = prefs.edit().putLong(PREF_KEY_TIME, value).apply()
 
 
 }
