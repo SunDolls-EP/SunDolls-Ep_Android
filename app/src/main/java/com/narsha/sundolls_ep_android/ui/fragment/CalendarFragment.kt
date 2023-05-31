5adb8697e6f9a7c17029dd021f9c876bcfd55cce
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-06-01 07:49:15 +0900

    :construction: Connect Viewmodel and DataBinding to Fragment

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/CalendarFragment.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/CalendarFragment.kt
@@ -1,13 +1,25 @@
 package com.narsha.sundolls_ep_android.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.narsha.sundolls_ep_android.R
+import com.narsha.sundolls_ep_android.databinding.FragmentCalendarBinding
+import com.narsha.sundolls_ep_android.databinding.FragmentFriendBinding
+import com.narsha.sundolls_ep_android.ui.viewmodel.fragment.CalendarViewModel
+import com.narsha.sundolls_ep_android.ui.viewmodel.fragment.FriendViewModel
 
 class CalendarFragment : Fragment() {
+
+    private val viewModel: CalendarViewModel by lazy {
+        ViewModelProvider(this)[CalendarViewModel::class.java]
+    }
+
+    lateinit var binding: FragmentCalendarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +28,11 @@ class CalendarFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    ): View {
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        binding.calendarViewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
 }
--- app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/SettingFragment.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/SettingFragment.kt
@@ -1,14 +1,28 @@
 package com.narsha.sundolls_ep_android.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.narsha.sundolls_ep_android.R
+import com.narsha.sundolls_ep_android.databinding.FragmentCalendarBinding
+import com.narsha.sundolls_ep_android.databinding.FragmentFriendBinding
+import com.narsha.sundolls_ep_android.databinding.FragmentSettingBinding
+import com.narsha.sundolls_ep_android.ui.viewmodel.fragment.CalendarViewModel
+import com.narsha.sundolls_ep_android.ui.viewmodel.fragment.SettingViewModel
 
 
 class SettingFragment : Fragment() {
+
+    private val viewModel: SettingViewModel by lazy {
+        ViewModelProvider(this)[SettingViewModel::class.java]
+    }
+
+    lateinit var binding: FragmentSettingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +31,14 @@ class SettingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        binding.settingViewModel = viewModel
+        binding.lifecycleOwner = this
+
+
+        return binding.root
     }
 
 }
