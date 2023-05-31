0b8d30f60dafece8a6954483b2badf62553b3768
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-31 16:48:14 +0900

    :white_check_mark: add OkHttpClient

--- app/src/main/java/com/narsha/sundolls_ep_android/data/local/retrofit/ClientRetrofit.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/data/local/retrofit/ClientRetrofit.kt
@@ -1,12 +1,17 @@
 package com.narsha.sundolls_ep_android.data.local.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
 object ClientRetrofit {
-    const val baseURL = "http://10.80.163.121:6974/"
+    private const val baseURL = "http://10.80.163.29:6974/"
 
+
+    val client = OkHttpClient.Builder()
+        .addInterceptor(HeaderInterceptor())
+        .build()
 
     val retrofit = Retrofit.Builder()
         .baseUrl(baseURL)
