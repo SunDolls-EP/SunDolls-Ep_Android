13666e1b7a9b0f9b0a9d43aaef73abb4b914cb04
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-05-31 17:33:38 +0900

    :construction: add Setting, Calendar Fragment

---
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/CalendarFragment.kt
@@ -0,0 +1,24 @@
+package com.narsha.sundolls_ep_android.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.narsha.sundolls_ep_android.R
+
+class CalendarFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+}
