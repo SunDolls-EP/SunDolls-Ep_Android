0ca9ada9085310551a73ee55a991f0ef21f273fa
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-09-20 20:37:58 +0900

    :sparkles: :: button click event change

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/activity/LoginOAuthViewModel.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/viewmodel/activity/LoginOAuthViewModel.kt
@@ -2,6 +2,7 @@ package com.narsha.sundolls_ep_android.ui.viewmodel.activity
 
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,32 +10,9 @@ import com.narsha.sundolls_ep_android.ui.activity.Home
 import com.narsha.sundolls_ep_android.ui.activity.Login
 
 class LoginOAuthViewModel: ViewModel() {
-
-    private val _googleRegistration = MutableLiveData<Boolean>()
-    private val _kakaoRegistration = MutableLiveData<Boolean>()
-    val googleRegistration: LiveData<Boolean>
-        get() = _googleRegistration
-    val kakaoRegistration: LiveData<Boolean>
-        get() = _kakaoRegistration
-    private val _nonRegistration = MutableLiveData<Boolean>()
-    val nonRegistration: LiveData<Boolean>
-        get() = _nonRegistration
-
     fun nextActivity() {
         val intent = Intent(Login.applicationContext(), Home::class.java)
-        Login.applicationContext().startActivity(intent.addFlags(FLAG_ACTIVITY_NEW_TASK))
-    }
-
-    fun nonRegistration(){
-        _nonRegistration.value = true
-    }
-
-    fun googleRegistration() {
-        _googleRegistration.value = true
-    }
-
-    fun kakaoRegistration(){
-        _kakaoRegistration.value = true
+        Login.applicationContext().startActivity(intent.addFlags(FLAG_ACTIVITY_NO_ANIMATION))
     }
 
 }
