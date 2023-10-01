2216d631845f24002e9ff3f4aea4f36850fcf80f
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-10-01 10:00:48 +0900

    :sparkles: add HomeActivity

---
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/activity/HomeActivity.kt
@@ -0,0 +1,61 @@
+package com.narsha.sundolls_ep_android.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.narsha.sundolls_ep_android.R
+import com.narsha.sundolls_ep_android.databinding.ActivityHomeBinding
+import com.narsha.sundolls_ep_android.ui.viewmodel.activity.HomeViewModel
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private val viewModel: HomeViewModel by lazy { ViewModelProvider(this)[HomeViewModel::class.java] }
+    private val binding: ActivityHomeBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_home
+        )
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.home = viewModel
+        binding.lifecycleOwner = this
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bnv.setupWithNavController(navController)
+
+//        binding.bnv.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.ranking -> {
+//
+//                }
+//                R.id.novel -> {
+//
+//                }
+//
+//                R.id.home -> {
+
+//                }
+//
+//                R.id.menu -> {
+//
+//                }
+//            }
+//            true
+//        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
