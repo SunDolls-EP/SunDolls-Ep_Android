3119322740a4dd31cae47a7ff2c8eff7b747f604
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-10-04 22:01:37 +0900

    :zap: :: add logout logic

--- app/src/main/java/com/narsha/sundolls_ep_android/data/model/MySharedPreferences.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/data/model/MySharedPreferences.kt
@@ -6,27 +6,29 @@ import android.content.SharedPreferences
 class MySharedPreferences(context: Context) {
     companion object{
         const val PREFS_FILENAME = "prefs"
-        const val PREF_KEY_ACCESSTOKEN = "accessToken"
-        const val PREF_KEY_TIME = "time"
-        const val PREF_KEY_START_TIME = "startTime"
-        const val PREF_KEY_END_TIME = "endTime"
+        const val PREF_KEY_ACCESSTOKEN = "PREF_KEY_ACCESSTOKEN"
+        const val PREF_KEY_NAME = "PREF_KEY_NAME"
+        const val PREF_KEY_SCHOOL = "PREF_KEY_SCHOOL"
     }
 
-    val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
-    var accessToken: String?
-        get() = prefs.getString(PREF_KEY_ACCESSTOKEN, "")
+    private val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
+    var accessToken: String
+        get() = prefs.getString(PREF_KEY_ACCESSTOKEN, "").toString()
         set(value) = prefs.edit().putString(PREF_KEY_ACCESSTOKEN, value).apply()
 
-    var time: Long
-        get() = prefs.getLong(PREF_KEY_TIME,0L)
-        set(value) = prefs.edit().putLong(PREF_KEY_TIME, value).apply()
+    var name: String
+        get() = prefs.getString(PREF_KEY_NAME, "").toString()
+        set(value) = prefs.edit().putString(PREF_KEY_NAME, value).apply()
 
-    var startTime: String?
-        get() = prefs.getString(PREF_KEY_START_TIME, "")
-        set(value) = prefs.edit().putString(PREF_KEY_START_TIME, value).apply()
+    var school: String
+        get() = prefs.getString(PREF_KEY_SCHOOL, "").toString()
+        set(value) = prefs.edit().putString(PREF_KEY_SCHOOL, value).apply()
 
-    var endTime: String?
-        get() = prefs.getString(PREF_KEY_END_TIME, "")
-        set(value) = prefs.edit().putString(PREF_KEY_END_TIME, value).apply()
+
+    fun deleteUserInformation() {
+        prefs.edit().remove(PREF_KEY_ACCESSTOKEN).apply()
+        prefs.edit().remove(PREF_KEY_NAME).apply()
+        prefs.edit().remove(PREF_KEY_SCHOOL).apply()
+    }
 
 }
--- app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/menu/MenuFragment.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/menu/MenuFragment.kt
@@ -1,16 +1,37 @@
 package com.narsha.sundolls_ep_android.ui.fragment.menu
 
+import android.content.Intent
+import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.fragment.app.viewModels
+import com.narsha.sundolls_ep_android.App
 import com.narsha.sundolls_ep_android.R
 import com.narsha.sundolls_ep_android.base.BaseFragment
 import com.narsha.sundolls_ep_android.databinding.FragmentMenuBinding
+import com.narsha.sundolls_ep_android.ui.activity.LoginActivity
 import com.narsha.sundolls_ep_android.ui.viewmodel.fragment.MenuViewModel
 
-class MenuFragment: BaseFragment<FragmentMenuBinding, MenuViewModel>(
+class MenuFragment : BaseFragment<FragmentMenuBinding, MenuViewModel>(
     R.layout.fragment_menu
 ) {
     override val viewModel: MenuViewModel by viewModels()
     override fun start() {
+        with(binding) {
+            name.text = App.prefs.name
+            school.text = App.prefs.school
 
+
+            buttonAccountFix.setOnClickListener {}
+            buttonLogout.setOnClickListener {
+                logout()
+            }
+        }
+    }
+
+    private fun logout(){
+        App.prefs.deleteUserInformation()
+        Intent(context, LoginActivity::class.java).also {
+            finishAffinity(requireActivity())
+            startActivity(it)
+        }
     }
 }
