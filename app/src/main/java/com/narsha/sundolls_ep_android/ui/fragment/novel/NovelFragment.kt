88f159af91d7750bc8d89cb11834c9688240ea11
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-11-08 20:42:57 +0900

    :zap: :: add novel recyclerview

--- app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/novel/NovelFragment.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/ui/fragment/novel/NovelFragment.kt
@@ -3,9 +3,12 @@ package com.narsha.sundolls_ep_android.ui.fragment.novel
 import android.util.Log
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.narsha.sundolls_ep_android.R
 import com.narsha.sundolls_ep_android.base.BaseFragment
 import com.narsha.sundolls_ep_android.databinding.FragmentNovelBinding
+import com.narsha.sundolls_ep_android.ui.adapter.UserListAdapter
+import com.narsha.sundolls_ep_android.ui.adapter.decoration.UserDecoraiton
 import com.narsha.sundolls_ep_android.ui.viewmodel.UserViewModel
 
 class NovelFragment: BaseFragment<FragmentNovelBinding, NovelViewModel>(
@@ -13,13 +16,30 @@ class NovelFragment: BaseFragment<FragmentNovelBinding, NovelViewModel>(
 ) {
     override val viewModel: NovelViewModel by viewModels()
     val userViewModel: UserViewModel by activityViewModels()
+
     override fun start() {
+        val userListAdapter = UserListAdapter(requireContext())
+        val userDecoraiton = UserDecoraiton()
+
+        userViewModel.getUserRandom(11)
+
         Log.d("UserInformation",userViewModel.userData.value.toString())
+
+
         with(binding){
+            recyclerviewFriend.adapter = userListAdapter
+            recyclerviewFriend.layoutManager = LinearLayoutManager(context)
+            recyclerviewFriend.addItemDecoration(userDecoraiton)
+
             buttonFind.setOnClickListener {
                 findUser(name.text.toString())
             }
         }
+
+        userViewModel.friendDataList.observe(this@NovelFragment){
+            userListAdapter.submitList(it)
+        }
+
     }
 
     private fun findUser(name: String){
