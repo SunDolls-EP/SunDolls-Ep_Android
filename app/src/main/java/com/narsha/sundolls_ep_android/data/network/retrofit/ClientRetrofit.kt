7d19539a983d2ca583b438dd6dfe3bc04068d0d8
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-11-08 20:43:32 +0900

    :zap: :: add logging Body

--- app/src/main/java/com/narsha/sundolls_ep_android/data/network/retrofit/ClientRetrofit.kt
+++ app/src/main/java/com/narsha/sundolls_ep_android/data/network/retrofit/ClientRetrofit.kt
@@ -1,6 +1,7 @@
 package com.narsha.sundolls_ep_android.data.network.retrofit
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,6 +9,7 @@ object ClientRetrofit {
     private const val baseURL = "http://dgswhs.gabia.io/"
 
     private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
         .addInterceptor(HeaderInterceptor())
         .build()
 
